55c7ad4bbe73d8b8d159df44aca2a2d613debe2c
Nicholas Hood <hoodn284@gmail.com>
2023-01-25 14:52:02 -0600

    finished the studio with anthony

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,48 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
-
+    @Test
+    public void onlyBracketsReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+@Test
+    public void leftBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+}
+@Test
+    public void rightBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+}
+@Test
+    public void endOfStringBracketsReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Mary had a little lamb []"));
+}
+@Test
+    public void beginningOfStringBracketsReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[] Mary had a little lamb"));
+}
+@Test
+    public void middleOfStringBracketsReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Mary had [a] little lamb"));
+}
+@Test
+    public void doubleBracketsEndBeginningReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[] Mary had a little lamb []"));
+}
+@Test
+    public void noClosingBracketsReturnFalse(){
+      assertFalse(BalancedBrackets.hasBalancedBrackets("[Mary had a little lamb"));
+}
+@Test
+    public void noOpeningBracketsReturnFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Mary had a little lamb]"));
+}
+@Test
+    public void oppositeDirectionBracketsReturnFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Mary had a little lamb ]["));
+}
+@Test
+    public void tripleBracketsReturnTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[Mary] had [a] little [lamb]"));
+}
 }
